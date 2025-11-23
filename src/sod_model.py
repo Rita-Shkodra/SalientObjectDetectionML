7a95f72f894105de4673c3bb1c8f7307d56d9e00
Rita-Shkodra <ritashkodra@gmail.com>
2025-11-23 16:29:46 +0000

    initial sod model structure

--- src/sod_model.py
+++ src/sod_model.py
@@ -1,1 +1,11 @@
- 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SODModel(nn.Module):
+    def __init__(self):
+        super(SODModel, self).__init__()
+   
+
+    def forward(self, x):
+        return x
