81cdd6c2e61116f9a6d0dec9e304b56a09bae624
Rita-Shkodra <ritashkodra@gmail.com>
2025-11-23 18:22:06 +0000

    adding encoder layers in sod_model

--- src/sod_model.py
+++ src/sod_model.py
@@ -5,7 +5,24 @@ import torch.nn.functional as F
 class SODModel(nn.Module):
     def __init__(self):
         super(SODModel, self).__init__()
-   
+        self.enc1 = nn.Sequential(
+          nn.Conv2d(3, 32, kernel_size=3, padding=1),
+          nn.ReLU(inplace=True),
+          nn.MaxPool2d(2)
+        )
+        self.enc2 = nn.Sequential(
+          nn.Conv2d(32, 64, kernel_size=3, padding=1),
+          nn.ReLU(inplace=True),
+          nn.MaxPool2d(2)
+        )
+        self.enc3 = nn.Sequential(
+          nn.Conv2d(64, 128, kernel_size=3, padding=1),
+          nn.ReLU(inplace=True),
+          nn.MaxPool2d(2)
+        )
 
     def forward(self, x):
+        x = self.enc1(x)
+        x = self.enc2(x)
+        x = self.enc3(x)
         return x
