23d975d08aad6a56fa28374cefb96ac9da622b04
Rita-Shkodra <ritashkodra@gmail.com>
2025-11-23 22:35:56 +0000

    Adding on train.py

--- src/train.py
+++ src/train.py
@@ -1,1 +1,15 @@
- 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from src.data_loader import get_dataloaders
+from src.sod_model import SODModel
+import numpy as np
+def iou(pred_mask, true_mask, threshold=0.5):
+    pred_mask = (pred_mask > threshold).float()
+    intersection = (pred_mask * true_mask).sum()
+    union = pred_mask.sum() + true_mask.sum() - intersection
+    if union == 0:
+        return torch.tensor(1.0)
+    else:
+        return intersection / union
